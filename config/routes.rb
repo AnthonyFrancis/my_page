0b470b8c18826a4b7abde3986e59846448655884
Anthony Francis <tonytrack@gmail.com>
2021-07-04 19:31:37 +0100

    add home page design

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#index"
+  get "blog" => "pages#blog"
+  get "books" => "pages#books"
+  get "news" => "pages#newsletter"
+  get "products" => "pages#products"
+  get "projects" => "pages#projects"
+  get "aboutme" => "pages#aboutme"
   # get 'pages/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
